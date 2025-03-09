93559b1bedf3ccd7b8a015f7eda018995778acc0
YounG-0516 <210110812.stu.hit.edu.cn>
2025-03-09 16:27:06 +0800

    0309

--- Prediction_ModernTCN/run.sh
+++ Prediction_ModernTCN/run.sh
@@ -438,10 +438,41 @@
 CUDA_VISIBLE_DEVICES=1 python -u run.py \
     --is_training 1 \
     --checkpoints "./checkpoints_M/" \
-    --model_id "珠江口伶仃以南" \
+    --model_id "大鹏湾沙头角" \
     --model ModernTCN \
     --root_path "../data_9features/" \
-    --data_path "珠江口伶仃以南_2023_2024.csv" \
+    --data_path "大鹏湾沙头角_2023_2024.csv" \
+    --data custom \
+    --features M \
+    --seq_len 336 \
+    --pred_len 24 \
+    --ffn_ratio 16 \
+    --patch_size 16 \
+    --patch_stride 4 \
+    --num_blocks 2 \
+    --large_size 51 \
+    --small_size 5 \
+    --dims 64 64 64 64 \
+    --head_dropout 0.0 \
+    --enc_in 9 \
+    --dropout 0.1 \
+    --itr 1 \
+    --train_epochs 10 \
+    --batch_size 512 \
+    --patience 20 \
+    --inverse \
+    --scale True \
+    --learning_rate 0.0001 \
+    --des Exp \
+    --lradj type3
+
+CUDA_VISIBLE_DEVICES=1 python -u run.py \
+    --is_training 1 \
+    --checkpoints "./checkpoints_M/" \
+    --model_id "大鹏湾沙头角" \
+    --model ModernTCN \
+    --root_path "../data_9features/" \
+    --data_path "大鹏湾沙头角_2023_2024.csv" \
     --data custom \
     --features M \
     --seq_len 336 \
